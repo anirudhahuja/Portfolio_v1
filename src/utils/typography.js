330a43f3177a491932421a30ae71ca019d4b2528
Anirudh <ani.ahuja219@gmail.com>
2020-07-07 13:06:31 -0700

    Fixed font imports

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,14 @@
-import Typography from "typography";
+// import Typography from "typography";
 
 
-const typography = new Typography({
-  baseFontSize: "25px",
-  baseLineHeight: 1.45,
-  headerFontFamily: ["Montserrat", "serif"],
-  bodyFontFamily: ["Roboto Slab", "sans-serif"]
-});
+// const typography = new Typography({
+//   baseFontSize: "25px",
+//   baseLineHeight: 1.45,
+//   headerFontFamily: ["Montserrat", "serif"],
+//   bodyFontFamily: ["Roboto Slab", "sans-serif"]
+// });
 
-// Insert styles directly into the <head>
-typography.injectStyles();
+// // Insert styles directly into the <head>
+// typography.injectStyles();
 
-export default typography;
+// export default typography;
